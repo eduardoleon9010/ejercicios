37511f71728fe9ba42133aaeafc5082b60c30a92
Ing. Eduardo León <66545075+eduardoleon9010@users.noreply.github.com>
2023-12-25 14:20:35 -0500

    Update tupla_1.py

--- tupla_1.py
+++ tupla_1.py
@@ -1,6 +1,23 @@
-# -*- coding: utf-8 -*-
+"""
+Este script se encarga de contar los votos para cada candidato (Donald Trump y Joe Biden) en una 
+lista de votos por estado. 
 
-def contar_votos_estado(votos:list, estado_interes:str)->tuple:
+Este script hace uso de dos funciones:
+contar_votos_estado: Cuenta los votos para Trump y Biden en un estado específico.
+contar_total_votos_por_estado: Calcula el total de votos para ambos candidatos en todos los estados.
+"""
+
+def contar_votos_estado(votos: list, estado_interes: str) -> tuple:
+    """
+    Cuenta los votos para cada candidato en un estado específico.
+
+    Parámetros:
+    votos (list): Lista de tuplas de votos, cada una con (id_voto, candidato, estado, condado).
+    estado_interes (str): El estado del cual se quieren contar los votos.
+
+    Retorna:
+    tuple: Tupla con la cantidad de votos para Trump y Biden en el estado de interés.
+    """
     cant_votos_trump = 0
     cant_votos_biden = 0
     
@@ -15,28 +32,32 @@ def contar_votos_estado(votos:list, estado_interes:str)->tuple:
                
     return (cant_votos_trump, cant_votos_biden)
 
-def contar_total_votos_por_estado(votos:list, estado:tuple)->dict:
+
+def contar_total_votos_por_estado(votos: list, estados: tuple) -> dict:
+    """
+    Cuenta el total de votos por estado para ambos candidatos.
+
+    Parámetros:
+    votos (list): Lista de tuplas de votos, cada una con (id_voto, candidato, estado, condado).
+    estados (tuple): Tupla de nombres de estados.
+
+    Retorna:
+    dict: Diccionario con el total de votos para Trump y Biden en cada estado.
+    """
     totales_estado = {}
-    for i in range(len(estados)):
-        estado_actual = estados[i]
+    for estado_actual in estados:
         votos_estado_actual = contar_votos_estado(votos, estado_actual)
         totales_estado[estado_actual] = votos_estado_actual
         
     return totales_estado
 
-import random
-estados = ('Alaska','Alabama', 'Arkansas', 'Arizona', 'California',
-           'Colorado', 'Connecticut', 'District of Columbia', 'Delaware',
-           'Florida', 'Georgia', 'Hawaii', 'Iowa', 'Idaho', 'Illinois',
-           'Indiana', 'Kansas', 'Kentucky', 'Lousiana', 'Massachusetts',
-           'Maryland', 'Maine', 'Michigan', 'Minnesota', 'Missouri', 
-           'Mississippi', 'Montana', 'Nort Carolina', 'Nort Dackota',
-           'Nebraska', 'New Hampshire', 'New Jersey', 'New Mexico', 'Nevada'
-           'New York', 'Ohio', 'Oklahoma', 'Oregon', 'Pensilvania',
-           'Rhode Island', 'Soud Carolina', 'Sout Dakota', 
-           'Tennessee', 'Texas', 'Utah', 'Virginia', 'Vermont', 'Washington',
-           'Wisconsin', 'West Virginia', 'Wyming')
 
+import random
+# Lista de estados
+estados = ('Alaska','Alabama', 'Arkansas', 'Arizona', 'California', 'Colorado', 'Connecticut', 'District of Columbia', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Iowa', 'Idaho', 'Illinois', 'Indiana', 'Kansas', 'Kentucky', 'Lousiana', 'Massachusetts', 'Maryland', 'Maine', 'Michigan', 'Minnesota', 'Missouri', 'Mississippi', 'Montana', 'Nort Carolina', 'Nort Dackota', 'Nebraska', 'New Hampshire', 'New Jersey', 'New Mexico', 'Nevada', 'New York', 'Ohio', 'Oklahoma', 'Oregon', 'Pensilvania', 'Rhode Island', 'Soud Carolina', 'Sout Dakota', 'Tennessee', 'Texas', 'Utah', 'Virginia', 'Vermont', 'Washington', 'Wisconsin', 'West Virginia', 'Wyming')
+
+
+# Generación de datos de votos aleatorios para cada estado
 votos = []
 for est in estados:
     cant_votos = random.randint(4000, 8000)
@@ -49,12 +70,13 @@ for est in estados:
         voto = (id_voto, candidato, est, "Some Country")
         votos.append(voto)
         
+# Cálculo del total de votos por estado para cada candidato
 cuenta = contar_total_votos_por_estado(votos, estados)
+
+# Imprimir los totales de votos por estado
 for estado in cuenta:
     print(estado, "\nTOTAL ->", "Trump: "+str(cuenta[estado][0]), 
           "Biden: "+str(cuenta[estado][1])+"\n")
-        
-        
         
         
         

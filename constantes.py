96f717459de724971e8a45e9cea53e78f65f924f
Ing. Eduardo León <66545075+eduardoleon9010@users.noreply.github.com>
2023-12-24 23:43:50 -0500

    Update constantes.py

--- constantes.py
+++ constantes.py
@@ -1,9 +1,28 @@
-# -*- coding: utf-8 -*-
+"""
+Este código imprime en la consola los valores de algunas
+constantes matemáticas proporcionadas por el módulo math de Python. Aquí está la documentación
+"""
+
 import math
+
+# Imprime el valor de pi
 print("El valor de pi:", math.pi)
 
+# Imprime el valor de e (número de Euler)
 print("El valor de e:", math.e)
 
+# Imprime el valor de infinito (∞)
 print("El valor de infinito:", math.inf)
 
+# Imprime el valor de un número indefinido (NaN - Not a Number)
 print("El valor de un número indefinido:", math.nan)
+
+"""
+math.pi: Devuelve el valor de la constante pi (π) con una precisión específica.
+math.e: Devuelve el valor del número de Euler (e).
+math.inf: Representa el infinito positivo.
+math.nan: Representa un número no definido (NaN - Not a Number), que se utiliza para representar 
+resultados indefinidos o no numéricos en cálculos matemáticos.
+Estas líneas de código simplemente muestran en la consola los valores de estas constantes 
+matemáticas predefinidas en el módulo math de Python.
+"""

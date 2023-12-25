9b00822fe9adc4c52122c49d13a1b59b6f9c31b9
Ing. Eduardo León <66545075+eduardoleon9010@users.noreply.github.com>
2023-12-25 14:33:06 -0500

    Update list_2.py

--- list_2.py
+++ list_2.py
@@ -1,5 +1,31 @@
-# -*- coding: utf-8 -*-
-def menor_posicion_lista(numeros:list)->int:
+"""
+Encuentra la posición del número más pequeño en una lista.
+
+Esta función, `menor_posicion_lista`, recibe una lista de números y devuelve la posición del número más 
+pequeño en la lista.
+
+Args:
+- numeros (list): Lista de números enteros.
+
+Returns:
+- int: La posición del número más pequeño en la lista.
+
+Ejemplo de uso:
+lista = [4, -1, -4, 5, 1, 6, 10, 17, 31, -14, -61]
+print(menor_posicion_lista(lista))  # Imprime la posición del número más pequeño.
+"""
+
+
+def menor_posicion_lista(numeros:list) -> int:
+    """
+    Encuentra la posición del número más pequeño en una lista.
+
+    Args:
+    - numeros (list): Lista de números enteros.
+
+    Returns:
+    - int: La posición del número más pequeño en la lista.
+    """
     indice = 0
     menor = numeros[0]
     
@@ -9,6 +35,7 @@ def menor_posicion_lista(numeros:list)->int:
             
     return indice
 
-lista = [4,-1,-4,5,1,6,10,17,31,-14,-61]
+# Ejemplo de uso
+lista = [4, -1, -4, 5, 1, 6, 10, 17, 31, -14, -61]
+print(menor_posicion_lista(lista))  # Imprime la posición del número más pequeño.
 
-print(menor_posicion_lista(lista))

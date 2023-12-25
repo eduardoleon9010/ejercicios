78776fc69d84f0dde8bd6d660bfdf5d7c17d1ac8
Ing. Eduardo León <66545075+eduardoleon9010@users.noreply.github.com>
2023-12-24 23:41:13 -0500

    Update condicionales_en_cascada.py

--- condicionales_en_cascada.py
+++ condicionales_en_cascada.py
@@ -1,25 +1,45 @@
-# -*- coding: utf-8 -*-
-def funcion_a()->None:
-    print("Usted ha escogido la opcion a del menu")
+"""
+Este código es un menú simple que presenta al usuario varias opciones y ejecuta una función asociada a la opción elegida.
+
+Primero, se definen cuatro funciones: funcion_a, funcion_b, funcion_c, y funcion_d. Cada una de estas funciones imprime 
+un mensaje diferente indicando la opción seleccionada.
+
+Luego, en el programa principal, se muestra un menú con cuatro opciones: a, b, c y d. El usuario debe ingresar una de 
+estas letras como su elección.
+
+El programa verifica la opción ingresada por el usuario y ejecuta la función correspondiente a esa opción. Si el usuario 
+ingresa "a", se llama a funcion_a, si ingresa "b", se ejecuta funcion_b, y así sucesivamente. Si se ingresa una opción no 
+válida, el programa muestra un mensaje de "Selección inválida".
+
+El código presenta un menú al usuario, le permite elegir entre diferentes opciones y ejecuta una acción 
+asociada a la opción seleccionada.
+"""
+
+def funcion_a() -> None:
+    """Imprime un mensaje indicando que se seleccionó la opción 'a' del menú."""
+    print("Usted ha escogido la opción a del menú")
     
-def funcion_b()->None:
-    print("Usted a escogido la opcion b del menu")
+def funcion_b() -> None:
+    """Imprime un mensaje indicando que se seleccionó la opción 'b' del menú."""
+    print("Usted ha escogido la opción b del menú")
     
-def funcion_c()->None:
-    print("Usted ha escogido la opcion c del menu")
+def funcion_c() -> None:
+    """Imprime un mensaje indicando que se seleccionó la opción 'c' del menú."""
+    print("Usted ha escogido la opción c del menú")
     
-def funcion_d()->None:
-    print("Usted ha escogido la opcion d del menu")
+def funcion_d() -> None:
+    """Imprime un mensaje indicando que se seleccionó la opción 'd' del menú."""
+    print("Usted ha escogido la opción d del menú")
     
-#PROGRAMA PRICIPAL
+# PROGRAMA PRINCIPAL
 
 print("Menu principal")
-print ("Opcion a")
-print ("Opcion b")
-print ("Opcion c")
-print  ("Opcion d")
+print("Opción a")
+print("Opción b")
+print("Opción c")
+print("Opción d")
 
-x = input("Seleccione su opcion: ")
+x = input("Seleccione su opción: ")
 
 if x == "a":
     funcion_a()
@@ -29,9 +49,5 @@ elif x == "c":
     funcion_c()
 elif x == "d":
     funcion_d()
-
 else:
-    print("Seleccion invalida")
-    
-    
-    
+    print("Selección inválida")

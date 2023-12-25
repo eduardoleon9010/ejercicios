204815824a4df466c1a68082593ee117ee59103b
Ing. Eduardo León <66545075+eduardoleon9010@users.noreply.github.com>
2023-12-24 23:34:26 -0500

    Update and rename ciclo-for-in.py to ciclo_for_in.py

--- ciclo-for-in.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-def es_numero_primo(numero:int)->bool:
-    primo = True
-    for i in range(2,numero):
-        if numero % i == 0:
-            primo = False
-    
-    return primo

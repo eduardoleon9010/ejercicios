3ff1f5e480e32db386a421e32075dd5c991539b4
Ing. Eduardo León <66545075+eduardoleon9010@users.noreply.github.com>
2023-12-24 23:37:37 -0500

    Update condicionales_consecutivas.py

--- condicionales_consecutivas.py
+++ condicionales_consecutivas.py
@@ -1,4 +1,13 @@
-# -*- coding: utf-8 -*-
+"""
+Este código solicita tres números al usuario y luego cuenta cuántos de esos números son pares. Realiza lo siguiente:
+
+Solicita tres números al usuario.
+Verifica si cada uno de estos números es par utilizando el operador de módulo %. Si el residuo de la división entre 
+el número y 2 es igual a cero, significa que es un número par.
+Incrementa el contador cuantos por cada número par encontrado.
+Imprime el número total de pares entre los tres números ingresados por el usuario.
+"""
+
 num1 = int (input("Digite un numero: "))
 num2 = int (input("Digite un segundo numero: "))
 num3 = int (input("Digite un tercer numero: "))
